a2cd9b29be4a29d95fb08fd9cf61ee4e323dd9ef
TecH260 <mr.tech260476@gmail.com>
2022-11-18 10:06:07 +0300

    update

---
+++ api/Rent.ts
@@ -0,0 +1,9 @@
+﻿import axiosAuth from 'app/axiosAuth';
+import { API_URL } from 'app/config';
+
+export const SET_RENT_URL = `${API_URL}/rent`;
+
+// set array with user token and car id
+export async function setCarRent(car_id: number) {
+  return axiosAuth.post(SET_RENT_URL, { cid: car_id });
+}
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -34,26 +34,29 @@ export default function Post({ post }: { post: IPostModel }) {
         )} */}
         <Container>
           <div className='post__body'>
-            <div className='post__main'>
-              <div className='post__header'>
-                <h1 className='post__title title'>Мы открылись!</h1>
-                <div className='post__date'>2022-08-29</div>
-              </div>
-              <div className='post__text'>
-                Я брать машина на сутки. Все класс. Парк хороший. Авто хороший.
-                Звонил им, взял машина, ездил на машина 10 часов все было
-                хорошо. Машина не ломаться, руль крутиться, коляса
-                поворачиваться. Потом яма была, в яму упала, я жив. Права теперь
-                нет, автопарк сказал, что машина должен вернуть, я машина
-                вернуть в состоянии плохой. Но парк сказал, что машина не
-                годится. Надо кредит брать, помогите кто нибудь лада купить
-              </div>
+            <div className='post__header'>
+              <div className='post__date'>2022-08-29</div>
+              <h1 className='post__title title'>Мы открылись!</h1>
             </div>
             <div className='post__img'>
               <img
                 src='https://lockpixel.ru/_next/image?url=https%3A%2F%2Fxn--80aaf7asgim.xn--80ae0bp6d.xn--p1ai%2Fimg%2F%2F100000%2FScreenshot_44.jpg&w=1920&q=75'
                 alt=''
               />
+            </div>
+            <div className='post__text'>
+              <p>
+                <strong>Я брать машина на сутки.</strong> Все класс. Парк
+                хороший. Авто хороший. Звонил им, взял машина, ездил на машина
+                10 часов все было хорошо. Машина не ломаться, руль крутиться,
+                коляса поворачиваться. Потом яма была, в яму упала, я жив.
+              </p>
+              <p>
+                <span style={{ textDecoration: 'underline' }}>Права</span>{' '}
+                теперь нет, автопарк сказал, что машина должен вернуть, я машина
+                вернуть в состоянии плохой. Но парк сказал, что машина не
+                годится. Надо кредит брать, помогите кто нибудь лада купить
+              </p>
             </div>
           </div>
         </Container>
